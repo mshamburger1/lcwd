4dc3dd2872bafbc81f54669aac9bf24bc33f514d
mshamburger1 <michael.shamburger@gmail.com>
2020-10-30 15:53:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import ContactMePage from "./pages/contact";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/contact" component={ContactMePage} exact />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
