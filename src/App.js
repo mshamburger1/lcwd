445a891a21ed1d8c05a535c8bbe0a08e3cfcbeca
mshamburger1 <michael.shamburger@gmail.com>
2020-10-30 15:55:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import ContactMePage from "./pages/contact";
 
