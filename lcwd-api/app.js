85b46cbd93fc8fd44c24f967d450a63cc7679283
mshamburger1 <michael.shamburger@gmail.com>
2020-11-01 18:29:31 -0600

    hidden api key

---
+++ lcwd-api/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const sendGrid = require("@sendgrid/mail");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // change later to only allow our
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/api", (req, res, next) => {
+  console.log(req.body);
+  res.send("API Status: Running");
+});
+
+app.post("/api/email", (req, res, next) => {
+  sendGrid.setApiKey(process.env.API_KEY);
+  const msg = {
+    to: "michael.shamburger@gmail.com",
+    from: req.body.email,
+    subject: "LCWD Contact",
+    text: req.body.message,
+  };
+  sendGrid
+    .send(msg)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log("errors", err);
+      res.status(401).json({
+        success: false,
+      });
+    });
+});
+
+app.listen(3030, "0.0.0.0");
